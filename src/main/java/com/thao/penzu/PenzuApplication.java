00ab6e1004a9a20db5fa8dc950a82fa78bc19522
thao9415 <thao.9415@gmail.com>
2019-12-16 15:16:38 +0700

    first commit

---
+++ src/main/java/com/thao/penzu/PenzuApplication.java
@@ -0,0 +1,13 @@
+package com.thao.penzu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenzuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PenzuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thao/penzu/PenzuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thao.penzu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenzuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
